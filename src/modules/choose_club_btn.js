312e26bf5e9d9894d5dc45457b98ccf39bcb73ef
Dmitry Kevdin <vdin2001@mail.ru>
2021-05-13 22:07:41 +0300

    gift popup v2

--- src/modules/choose_club_btn.js
+++ src/modules/choose_club_btn.js
@@ -1,7 +1,12 @@
 "use strict";
 
 const chooseClubButton = (target) => {
-    target.closest('.club-select').querySelector('.clubs-list ul').style.display = 'block';
+    const clubSelectList = target.closest('.club-select').querySelector('.clubs-list ul');
+    if (clubSelectList.style.display === 'block') {
+        clubSelectList.style.display = 'none';
+    } else {
+        clubSelectList.style.display = 'block';
+    }
 };
 
 export default chooseClubButton;
